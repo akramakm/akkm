1b3f51c86e28418b8ff9a53860c24bd4a1a30918
akramakm <89606392+akramakm@users.noreply.github.com>
2022-06-12 19:09:34 +0100

    okok

---
+++ jaa3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("1 = dalami\n");
+    printf("2 = akram\n");
+    printf("3 = sami\n");
+    scanf("%i",&a);
+    if (a == 1)
+    {
+        printf("dalami yak r gha a saml hadak\n");
+    }
+    else
+    {
+        printf("ta rj3 tqwd ldalami \n");
+    }
+}
